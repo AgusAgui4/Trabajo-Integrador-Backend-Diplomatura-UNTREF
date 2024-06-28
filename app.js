390e5dbb4ac56c85b74230143b3274e96a27f394
AgusAgui4 <agustinaguilar1604@gmail.com>
2024-06-28 01:08:17 -0300

    endpoint que trae todos los dispositivos y le podes especificar una categoria

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const connectDB = require('./src/mongoose')
+const connectDB = require('./src/database')
 const port = process.env.PORT ?? 3000
 const morgan = require('morgan')
-const Movie = require('./src/movieModel')
+const product = require('./src/product')
 
 // Conectar a MongoDB usando Mongoose
 connectDB()
@@ -14,8 +14,30 @@ app.use(morgan('dev'))
 
 //Ruta principal
 app.get('/', (req, res) => {
-  res.json('Bienvenido a la API de peliculas !')
+  res.json('Bienvenido a la API de electronica !')
 })
+
+
+//trae todos los dispositivos y le podes especificar una categoria
+app.get('/dispositivos', (req, res) => {
+  const {categoria} = req.query
+  const query = !categoria ? {} : {categorias: {$eq: categoria }}
+  product.find(query)
+    .then((dispositivos) => {
+      res.json(dispositivos)
+    })
+    .catch((error) => {
+      console.error('Error al obtener dispositivos: ', error)
+      res.status(500).send('Error al obtener las dispositivos: ')
+    })
+})
+
+
+
+
+
+
+
 
 
 
