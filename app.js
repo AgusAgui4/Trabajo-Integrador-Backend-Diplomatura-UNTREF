7a1ad3c37bece856d059bb43852b0567371550ad
AgusAgui4 <agustinaguilar1604@gmail.com>
2024-07-26 21:39:53 -0300

    se agrego la verificacion para acceder a algunos sitios

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const connectDB = require("./src/database");
 const port = process.env.PORT ?? 3000;
 const morgan = require("morgan");
 const product = require("./src/product");
+const jwt = require('jsonwebtoken')
+const secretKey = process.env.SECRET_KEY
+const usuario = require('./src/users')
+
+
 
 // Conectar a MongoDB usando Mongoose
 connectDB();
@@ -11,6 +16,71 @@ connectDB();
 //Middleware
 app.use(express.json());
 app.use(morgan("dev"));
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.decoded = decoded;
+    
+    next();
+    
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body
+  console.log(`Datos recibidos: usuario: ${username}, password: ${password}`)
+  // Autenticacion del usuario
+  const user = usuario.find(
+     { username: { $eq: username } , password: { $eq: password }}  
+  )
+  if (!user) {
+    return res.status(401).send({ error: 'Credenciales invalidas' })
+  } else {
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '2m' })
+    return res.json({ token })
+  }
+})
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Verificar si el usuario ya existe en la base de datos
+    const existingUser = await usuario.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    // Crear un nuevo usuario
+    const newUser = new usuario({
+      username,
+      password,
+    });
+
+    // Guardar el usuario en la base de datos
+    await newUser.save();
+
+    // Generar un token JWT con una expiración de 2 minutos
+    const token = jwt.sign({ username: newUser.username }, 'your_secret_key', { expiresIn: '2m' });
+
+    // Devolver una respuesta exitosa con el token
+    res.json({ token }); 
+
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 
 //Ruta principal
 app.get("/", (req, res) => {
@@ -18,7 +88,7 @@ app.get("/", (req, res) => {
 });
 
 //trae todos los dispositivos y le podes especificar una categoria
-app.get("/dispositivos", (req, res) => {
+app.get("/dispositivos",verifyToken, (req, res) => {
   const { categoria } = req.query;
   const query = !categoria ? {} : { categorias: { $eq: categoria } };
   product
@@ -42,12 +112,12 @@ app.get("/dispositivos/:id", (req, res) => {
       if (dispositivos) {
         res.json(dispositivos);
       } else {
-        res.status(404).json({ message: "Peli no encontrada" });
+        res.status(404).json({ message: "producto no encontrado" });
       }
     })
     .catch((error) => {
-      console.error("Error al obtener la pelicula: ", error);
-      res.status(500).send("Error al obtener la pelicula");
+      console.error("Error al obtener el producto: ", error);
+      res.status(500).send("Error al obtener el producto");
     });
 });
 
@@ -82,10 +152,10 @@ app.get("/dispositivos/codigo/:codigo", (req, res) => {
 
 
 
-
+//busca por nombre
 app.get("/dispositivos/nombre/:nombre", (req, res) => {
   const { nombre } = req.params;
-  const query = { nombre: { $regex: "^" + nombre , "$options": "i" } };
+  const query = { nombre: { $regex: "^" + nombre , $options: "i" } };
   product
     .find(query)
     .then((respuesta) => {
@@ -114,7 +184,7 @@ app.post("/dispositivos/agregar/", (req, res) => {
 })
 
 
-
+//modifica precio buscando por codigo
 app.patch("/dispositivos/modificar/:codigo", (req, res) => {
   const  {codigo}  = req.params;
   const  modificacion  = req.body.precio;
@@ -139,6 +209,8 @@ app.patch("/dispositivos/modificar/:codigo", (req, res) => {
   }
 });
 
+
+//elimina por codigo
 app.delete("/dispositivos/delete/:codigo", (req, res) => {
   const { codigo } = req.params;
 
@@ -162,6 +234,7 @@ app.delete("/dispositivos/delete/:codigo", (req, res) => {
 });
 
 
+
 app.use ( (req, res) => {
   res.status(404).send("Página no encontrada");
 });
@@ -175,34 +248,4 @@ app.listen(port, () => {
 
 
 
-/* app.get("/dispositivos/codigo/:codigo", (req, res) => {
-  try {
-    const { codigo } = req.params;
 
-    if (!Number(codigo)) {
-      throw new Error("parametro invalido")
-    }
-
-    const parse = parseInt(codigo);
-    console.log(codigo);
-    const query = !codigo ? {} : { codigo: { $eq: parse } };
-
-    product
-      .find(query)
-      .then((dispositivos) => {
-        
-        if (!dispositivos.length) {
-          throw new Error("objeto no encontrado")
-        }
-        
-        res.json(dispositivos);
-       
-      })
-    
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-  
-});
- */
-//Inicializamos el servidor
